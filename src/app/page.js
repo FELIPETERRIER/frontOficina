b73b02b03025ff6801cc5b1795e9f2acb4301847
Felipe Bitencourt <felipeterrier@yahoo.com.br>
2024-05-20 20:15:05 -0300

    input senha

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,8 @@ import Gears from "../../public/images/gears.png"
 export default function Login() {
     const [user,setUser] = useState("");
     const[ident,setIdent] = useState("");
+    const[password,setPass] = useState("");
+
 
 
     const handleSubmit = () =>{
@@ -23,16 +25,23 @@ export default function Login() {
             </div>
             <div className={styles.users}>
                 <label>Usuário</label>
-                <select className={styles.input} type="text" onChange={(e) => setUser(e.target.value)}> 
-                    <option>anana</option>
-                    <option>nana</option>
-                    <option>na</option>
+                <select className={styles.input} id="user" type="text" onChange={(e) => setUser(e.target.value)}>  
+                    <option>Selecione o tipo de usuário</option>                   
+                    <option>Cliente</option>
+                    <option>Mecânico(a)</option>
+                    <option>Gerente</option>
+                    <option>Funileiro(a)</option>
+                    <option>Propietário(a)</option>
                 </select>       
             </div>
             <div className={styles.identification}>
                 <label>CPF</label>
-                <InputMask className={styles.input}  onChange={(e)=> setIdent(e.target.value)} mask="999.999.999 - 99"  maskChar="_" required/>        
-            </div>      
+                <InputMask className={styles.input} id="cpf"  onChange={(e)=> setIdent(e.target.value)} mask="999.999.999 - 99"  maskChar="_" required/>        
+            </div>
+            <div className={styles.password}>
+                <label>Senha</label>
+                <input type="password" id="password" className={styles.input}  onChange={(e)=> setPass(e.target.value)} placeholder="*********"required/>        
+            </div>        
             <div className={styles.loginButton}>        
                 <button onClick={handleSubmit}>Login</button>        
             </div>
